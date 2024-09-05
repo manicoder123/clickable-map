37b1783fff09793144c356e5a4fad250c56f294e
Digital Wizards zone <143681736+manicoder123@users.noreply.github.com>
2024-09-05 19:39:47 +0500

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -1,354 +1,618 @@
 var simplemaps_usmap_mapdata={
   main_settings: {
    //General settings
-    width: "650", //'700' or 'responsive'
+    width: "responsive", //'700' or 'responsive'
     background_color: "#FFFFFF",
     background_transparent: "yes",
-    
-    //Label defaults
-    label_color: "grey",
-    hide_labels: "no",
-    border_color: "#FFFFFF",
+    border_color: "#ffffff",
+    popups: "detect",
     
     //State defaults
-    state_description: "A US State",
-    state_color: "skyblue",
-    state_hover_color: "lightblue",
-    state_url: "http://simplemaps.com",
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
     all_states_inactive: "no",
+    all_states_zoomable: "yes",
     
     //Location defaults
-    location_description: "Location description",
-    location_color: "#FF0067",
-    location_opacity: "0.8",
-    location_url: "http://simplemaps.com",
-    location_size: "20",
-    location_type: "circle",
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
     all_locations_inactive: "no",
-    url_new_tab: "yes",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
     auto_load: "yes",
-   
-    //Zoom settings
-    zoom: "yes",
-    initial_zoom: "-1",
-    initial_zoom_solo: "yes",
-    popups: "detect",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
     state_image_url: "",
     state_image_position: "",
-    all_states_zoomable: "",
-    location_image_url: "",
-    label_hover_color: "",
-    label_size: "",
-    label_font: ""
+    location_image_url: ""
   },
   state_specific: {
     HI: {
       name: "Hawaii",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-hawaii"
     },
     AK: {
       name: "Alaska",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-alaska"
     },
     FL: {
       name: "Florida",
-      inactive: "no",
-      hide: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-florida"
     },
     NH: {
       name: "New Hampshire",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-new-hampshire"
     },
     VT: {
       name: "Vermont",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-vermont"
     },
     ME: {
       name: "Maine",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-maine"
     },
     RI: {
       name: "Rhode Island",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-rhode-island"
     },
     NY: {
       name: "New York",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-new-york"
     },
     PA: {
       name: "Pennsylvania",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-pennsylvania"
     },
     NJ: {
       name: "New Jersey",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-new-jersey"
     },
     DE: {
       name: "Delaware",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-delaware"
     },
     MD: {
       name: "Maryland",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-maryland"
     },
     VA: {
       name: "Virginia",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-virginia"
     },
     WV: {
       name: "West Virginia",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-west-virginia"
     },
     OH: {
       name: "Ohio",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-ohio"
     },
     IN: {
       name: "Indiana",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/indiana"
     },
     IL: {
       name: "Illinois",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/illinois"
     },
     CT: {
       name: "Connecticut",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-connecticut"
     },
     WI: {
       name: "Wisconsin",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-wisconsin"
     },
     NC: {
       name: "North Carolina",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-north-carolina"
     },
     DC: {
-      name: "District of Columbia",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     },
     MA: {
       name: "Massachusetts",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-massachusetts"
     },
     TN: {
       name: "Tennessee",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-tennessee"
     },
     AR: {
       name: "Arkansas",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-arkansas"
     },
     MO: {
       name: "Missouri",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-missouri"
     },
     GA: {
       name: "Georgia",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-georgia"
     },
     SC: {
       name: "South Carolina",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-south-carolina"
     },
     KY: {
       name: "Kentucky",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-kentucky"
     },
     AL: {
       name: "Alabama",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-alabama"
     },
     LA: {
       name: "Louisiana",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-louisian"
     },
     MS: {
       name: "Mississippi",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-mississippi"
     },
     IA: {
       name: "Iowa",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-Iowa"
     },
     MN: {
       name: "Minnesota",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-minnesota"
     },
     OK: {
       name: "Oklahoma",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-oklahoma"
     },
     TX: {
       name: "Texas",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-texas"
     },
     NM: {
       name: "New Mexico",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-new-mexico"
     },
     KS: {
       name: "Kansas",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-kansas"
     },
     NE: {
       name: "Nebraska",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-nebraska"
     },
     SD: {
       name: "South Dakota",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-south-dakota"
     },
     ND: {
       name: "North Dakota",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-north-dakota"
     },
     WY: {
       name: "Wyoming",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-wyoming"
     },
     MT: {
       name: "Montana",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-montana"
     },
     CO: {
       name: "Colorado",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-colorado"
     },
     UT: {
       name: "Utah",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-utah"
     },
     AZ: {
       name: "Arizona",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/home-care-license-in-arizona"
     },
     NV: {
       name: "Nevada",
-      hide: "no",
-      inactive: "no"
+      url: "https://offerings.homecareagencyblueprint.com/nevada"
     },
     OR: {
       name: "Oregon",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-oregon"
     },
     WA: {
       name: "Washington",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-washington"
     },
     CA: {
       name: "California",
-      hide: "no",
-      inactive: "no",
       url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-california"
     },
     MI: {
       name: "Michigan",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-michigan"
     },
     ID: {
       name: "Idaho",
-      hide: "no",
-      inactive: "no"
+      url: "https://homecareblueprint.mykajabi.com/start-a-home-care-business-in-idaho"
     },
     GU: {
-      name: "Guam",
-      description: "A US Territory",
-      color: "red",
-      hover_color: "darkred",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     },
     VI: {
-      name: "Virgin Islands",
-      description: "A US Territory",
-      color: "red",
-      hover_color: "darkred",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     },
     PR: {
-      name: "Puerto Rico",
-      description: "A US Territory",
-      color: "red",
-      hover_color: "darkred",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     },
     MP: {
-      name: "Northern Mariana Islands",
-      description: "A US Territory",
-      color: "red",
-      hover_color: "darkred",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     },
     AS: {
-      name: "American Samoa",
-      description: "A US Territory",
-      color: "red",
-      hover_color: "darkred",
-      hide: "no",
-      inactive: "no"
+      hide: "yes"
     }
   },
-  locations: {},
-  regions: {},
-  labels: {},
+  locations: {
+    "0": {
+      name: "New York City",
+      lat: 40.7143528,
+      lng: -74.0059731
+    },
+    "1": {
+      name: "Anchorage",
+      lat: 61.2180556,
+      lng: -149.9002778
+    }
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
   legend: {
     entries: []
-  }
+  },
+  regions: {}
 };
